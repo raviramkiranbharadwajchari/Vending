387c2b3006c00329c18534ec6aee38c8c67ab64c
ravir <raviram69358@gmail.com>
2024-02-10 14:58:10 +0530

    Task

--- Meghal_Vending/Vending/src/main/java/com/Vending/Entity/Coin.java
+++ Meghal_Vending/Vending/src/main/java/com/Vending/Entity/Coin.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
